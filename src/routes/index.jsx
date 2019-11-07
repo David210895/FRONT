312536181180bd466571379ed74e0f915e03269e
sebastian yabiku <sby2710@gmail.com>
2019-11-07 16:02:00 -0500

    it added changes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { LoginContainer } from '../features/auth';
-import { RoutingHome } from '../features/home';
-import { RegisterContainer } from '../features/register';
-import { Mentors } from '../features/mentors';
-import { DetailMentor } from '../features/detailMentor';
-import { Videos, VideoDetail } from '../features/videos';
-import { ProfileMentor } from '../features/profileMentor';
+import React from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import { LoginContainer } from "../features/auth";
+import { RoutingHome } from "../features/home";
+import { RegisterContainer } from "../features/register";
+import { Mentors } from "../features/mentors";
+import { DetailMentor } from "../features/detailMentor";
+import { Videos, VideoDetail } from "../features/videos";
+import { ProfileMentor } from "../features/profileMentor";
 /* import { HomeContainer } from '../features/home'; */
 
-
 function RoutingRoot() {
-
-    return (
-        <Router>
-            <Switch>
-                <Route exact path='/home' component={RoutingHome} />
-                <Route exact path='/login' component={LoginContainer} />
-                <Route exact path='/register' component={RegisterContainer} />
-                <Route exact path='/mentors/:id' component={DetailMentor} />
-                <Route exact path='/mentors' component={Mentors} />
-                <Route exact path='/videos' component={Videos}/>
-                <Route path='/curso/:id' component={VideoDetail} />
-                <Route path='/busqueda' component={ProfileMentor} />
-                <Redirect from='/' to='/home' />
-            </Switch>
-        </Router>
-    )
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/home" component={RoutingHome} />
+        <Route exact path="/login" component={LoginContainer} />
+        <Route exact path="/register" component={RegisterContainer} />
+        <Route exact path="/mentors/:id" component={DetailMentor} />
+        <Route exact path="/mentors" component={Mentors} />
+        <Route exact path="/videos" component={Videos} />
+        <Route path="/curso/:id" component={VideoDetail} />
+        <Route path="/busqueda" component={ProfileMentor} />
+        <Redirect from="/" to="/home" />
+      </Switch>
+    </Router>
+  );
 }
-export {
-    RoutingRoot
-}
+export { RoutingRoot };
